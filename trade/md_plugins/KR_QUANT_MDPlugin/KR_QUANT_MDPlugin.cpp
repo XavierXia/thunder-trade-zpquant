1d9a5fa7035abac5c28156395af278f18054bef6
XavierXia <xiawenxing2010@163.com>
2019-05-17 23:46:46 +0800

    修复bug

--- trade/md_plugins/KR_QUANT_MDPlugin/KR_QUANT_MDPlugin.cpp
+++ trade/md_plugins/KR_QUANT_MDPlugin/KR_QUANT_MDPlugin.cpp
@@ -158,6 +158,9 @@ bool CKrQuantMDPluginImp::Start()
         return false;
     }
 
+    //test
+    OnWaitOnMsg();
+
 	std::unique_lock<std::mutex> lk(m_mtxLoginSignal);
 	m_cvLoginSignalCV.wait_for(lk,std::chrono::seconds(10));
 	if (m_boolIsOnline)
